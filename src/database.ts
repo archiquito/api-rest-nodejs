31e73d2e084c1ad52a352489c1f0f1f9a0d83ec1
archiquito <archiquito@gmail.com>
2025-04-24 18:24:31 -0300

    adjust to deploy

--- src/database.ts
+++ src/database.ts
@@ -1,16 +1,17 @@
-import { Knex, knex as setupKenex } from 'knex'
-import { env } from './env'
+import { Knex, knex as setupKenex } from "knex";
+import { env } from "./env";
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: "sqlite",
+  connection:
+    env.DATABASE_CLIENT === "sqlite"
+      ? { filename: env.DATABASE_URL }
+      : env.DATABASE_URL,
   migrations: {
-    extension: 'ts',
-    directory: './db/migrations',
+    extension: "ts",
+    directory: "./db/migrations",
   },
   useNullAsDefault: true,
-}
+};
 
-export const knex = setupKenex(config)
+export const knex = setupKenex(config);
--- src/env/index.ts
+++ src/env/index.ts
@@ -1,26 +1,27 @@
-import { config } from 'dotenv'
-import { z } from 'zod'
+import { config } from "dotenv";
+import { z } from "zod";
 
-if(process.env.NODE_ENV === 'test'){
-  config({ path: '.env.test'})
+if (process.env.NODE_ENV === "test") {
+  config({ path: ".env.test" });
 } else {
-  config({ path: '.env'})
+  config({ path: ".env" });
 }
 
 const envSchema = z.object({
   NODE_ENV: z
-    .enum(['development', 'test', 'production'])
-    .default('development'),
+    .enum(["development", "test", "production"])
+    .default("development"),
+  DATABASE_CLIENT: z.enum(["sqlite", "pg"]),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
-})
+  PORT: z.coerce.number().default(3333),
+});
 
-const _env = envSchema.safeParse(process.env)
+const _env = envSchema.safeParse(process.env);
 
 if (_env.success === false) {
-  console.error('⚠️  Invalid environment variables!', _env.error.format())
+  console.error("⚠️  Invalid environment variables!", _env.error.format());
 
-  throw new Error(`Invalid environment variables!`)
+  throw new Error(`Invalid environment variables!`);
 }
 
-export const env = _env.data
+export const env = _env.data;
