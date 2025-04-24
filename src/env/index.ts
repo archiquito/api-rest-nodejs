cc7178989c3012be07317e0fa216c324407edc31
archiquito <archiquito@gmail.com>
2025-04-24 19:04:25 -0300

    adjustments

--- src/env/index.ts
+++ src/env/index.ts
@@ -10,8 +10,8 @@ if (process.env.NODE_ENV === "test") {
 const envSchema = z.object({
   NODE_ENV: z
     .enum(["development", "test", "production"])
-    .default("development"),
-  DATABASE_CLIENT: z.enum(["sqlite", "pg"]),
+    .default("production"),
+  DATABASE_CLIENT: z.enum(["sqlite", "pg"]).default('sqlite'),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
 });
