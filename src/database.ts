949d340588388ef8cea7f3d393dcca7cf02ea50b
archiquito <archiquito@gmail.com>
2025-04-25 11:43:25 -0300

    adjust env client

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import { Knex, knex as setupKenex } from "knex";
 import { env } from "./env";
 
 export const config: Knex.Config = {
-  client: "sqlite",
+  client: env.DATABASE_CLIENT,
   connection:
     env.DATABASE_CLIENT === "sqlite"
       ? { filename: env.DATABASE_URL }
