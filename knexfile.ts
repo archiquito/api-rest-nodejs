cd424a5e82cc4cd19880aeaf532b0ecb10778e25
archiquito <archiquito@gmail.com>
2025-04-25 11:55:07 -0300

    adjust knex

--- knexfile.ts
+++ knexfile.ts
@@ -1,3 +1,17 @@
-import { config } from "./src/database";
+import { Knex } from "knex";
+import { env } from "./src/env";
+
+export const config: Knex.Config = {
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === "sqlite"
+      ? { filename: env.DATABASE_URL }
+      : env.DATABASE_URL,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations",
+  },
+  useNullAsDefault: true,
+};
 
 export default config;
--- src/database.ts
+++ src/database.ts
@@ -1,17 +1,4 @@
-import { Knex, knex as setupKenex } from "knex";
-import { env } from "./env";
-
-export const config: Knex.Config = {
-  client: env.DATABASE_CLIENT,
-  connection:
-    env.DATABASE_CLIENT === "sqlite"
-      ? { filename: env.DATABASE_URL }
-      : env.DATABASE_URL,
-  migrations: {
-    extension: "ts",
-    directory: "./db/migrations",
-  },
-  useNullAsDefault: true,
-};
+import { knex as setupKenex } from "knex";
+import { config } from '../knexfile';
 
 export const knex = setupKenex(config);
